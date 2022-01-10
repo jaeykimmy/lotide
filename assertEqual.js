327893aee435d15424671fe5d2a140ed5b2a9f3f
Jaeyoung Kim <j.jaeykim@gmail.com>
2022-01-10 16:39:57 -0500

    changed from concatenation to template literal

--- assertEqual.js
+++ assertEqual.js
@@ -1,14 +1,14 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("💯💯💯 Assertion Passed: " + actual + " === " + expected);
+    console.log(`💯💯💯 Assertion Passed: ${actual} === ${expected}`);
   } else if (actual !== expected) {
-    console.log("😐😐😐 Assertion Failed: " + actual + " !== " + expected);
+    console.log(`😐😐😐 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
 // TEST CODE
 assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+assertEqual('1', 1);
 assertEqual("eyeball", "eyeball");
 assertEqual(2, 1);
