fbdfcf170d71da93cbb4aeb69d09831270bcac03
Jaeyoung Kim <j.jaeykim@gmail.com>
2022-01-12 19:53:09 -0500

    object equality function added

--- eqArrays.js
+++ eqArrays.js
@@ -1,4 +1,8 @@
 const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false
+  }
+const eqArrays = function(arr1, arr2) {
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
       return false;
