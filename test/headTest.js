1636894218288988d7d6ecafec36798b8f0824d6
Jaeyoung Kim <j.jaeykim@gmail.com>
2022-01-17 13:40:14 -0500

    tail function refactored to have test file and assertEqual exported

--- test/headTest.js
+++ test/headTest.js
@@ -1,4 +1,4 @@
 //Test Code
-const assertEqual = require('../assertEqual')
-const head = require('../head')
+const assertEqual = require('../assertEqual');
+const head = require('../head');
 assertEqual(head([5,6,7]), 5);
